d7268e550225eb3df1231acbf3bf5ad63f64343e
Jesus240628 <jesus.quintero240628@potros.itson.edu.mx>
2022-11-28 16:18:13 -0700

    version inicial del proyecto ramen

---
+++ src/mx/itson/ramen/entidades/Estado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.ramen.entidades;
+
+import java.util.List;
+
+
+
+/**
+ *
+ * @author Jesus Javier-240628
+ */
+public class Estado {
+    private String producto;
+    private int cuenta;
+    private String moneda;
+    private String cliente;
+    private List <Transferencia> transferencias;
+}
